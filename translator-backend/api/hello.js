51d509726b2618ddd2c86b0d61eeefb0c997f475
appstion <yourrr313@gmail.com>
2025-08-24 23:30:46 +0700

    Delete translator-backend directory

--- translator-backend/api/hello.js
+++
@@ -1,5 +0,0 @@
-// api/hello.js
-module.exports = (req, res) => {
-  res.setHeader('Content-Type', 'application/json; charset=utf-8');
-  res.status(200).json({ ok: true, time: new Date().toISOString() });
-};

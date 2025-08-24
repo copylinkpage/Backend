cc0e14e1e19f40610dd175a3548a61ae011df0a5
appstion <yourrr313@gmail.com>
2025-08-24 23:50:44 +0700

    Update hello.js

--- api/hello.js
+++ api/hello.js
@@ -1,6 +1,6 @@
-// api/hello.js
 module.exports = (req, res) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  if (req.method === 'OPTIONS') return res.status(204).end();
   res.setHeader('Content-Type', 'application/json; charset=utf-8');
   res.status(200).json({ ok: true, time: new Date().toISOString() });
-
 };
